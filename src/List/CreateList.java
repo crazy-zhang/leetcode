06c93b9345d016aa2edabb8775b96bb02dc4a638
yongqiang <1294955726@qq.com>
2020-06-18 16:29:06 +0800

    java链表头插和尾插

---
+++ src/List/CreateList.java
@@ -0,0 +1,60 @@
+package List;
+
+public class CreateList {
+
+    public static class LinkList{
+        private int data;
+        private LinkList next;
+        //头插法
+        public LinkList top_create(int len) {
+            LinkList head = null;
+
+            for(int i = 0; i < len; i++) {
+                LinkList temp = new LinkList();
+                temp.data = i;
+                if(head == null){
+                    head = temp;
+                }
+                else{
+                    temp.next = head;
+                    head = temp;
+                   // System.out.println(i);
+                }
+            }
+            return head;
+        }
+
+        //尾插法  一定要加一个尾指针，遍历的时候需要从头到尾，不然一直插到尾
+        public LinkList tail_create(int len) {
+            LinkList head = null;
+            LinkList tail = null;
+
+            for(int i = 0; i < len; i++) {
+                LinkList temp = new LinkList();
+                temp.data = i;
+
+                if(head == null) {
+                    head = tail = temp;
+                }
+                else {
+                    tail.next = temp;
+                    tail = temp;
+                    //System.out.println(temp.data);
+                }
+            }
+            return head;
+        }
+        public void display(LinkList head) {
+            LinkList list = head;
+            while(list != null) {
+                System.out.println(list.data);
+                list = list.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list = list.top_create(10);
+        list.display(list);
+    }
+}
